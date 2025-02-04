25de676789b8521b4812ccf729df99e9baf48c69
marine <kakasska1212@gmail.com>
2025-02-05 03:13:13 +0900

    HellowJava

---
+++ src/CommemtJava.java
@@ -0,0 +1,13 @@
+public class CommemtJava {
+    public static void main(String[] args) { //psvm+enter
+        System.out.println("hello java1"); 
+        // sout+enter // hello java1을 무시합니다 > 이줄은 무시됨 한줄 주석
+        // System.out.println("hello java2");
+
+        /* 여러줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        System.out.println("hello java5");
+         */
+    }
+}
