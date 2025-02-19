2c95cc2add912ed97ed88561a9abd7824b63735d
marine <kakasska1212@gmail.com>
2025-02-20 02:22:26 +0900

    A-3_variable1

---
+++ src/A_variable/Var2.java
@@ -0,0 +1,9 @@
+package A_variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a ; // 변수 선언 int = 정수
+        a = 10; // 변수 초기화 > 10을 a라는 그릇에 담는다. 초기화 = 선언한 변수에 처음으로 값을 대입해서 저장하는 것
+        System.out.println(a); // a에 담은 10을 불러낸다. // 즉 값을 한번에 바꾸기 쉬운게 변수!
+    }
+}
