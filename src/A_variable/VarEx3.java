b65b269af4fd7b2874fb8113ea18b0ad2d185b87
kimsiyeonA <kakasska1212@gmail.com>
2025-03-05 14:34:49 +0900

    A_6Variable_명명규칙_문제풀이

---
+++ src/A_variable/VarEx3.java
@@ -0,0 +1,12 @@
+package A_variable;
+
+//  쉬움
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 1000000000L;
+        boolean b = true;
+
+        System.out.println(l);
+        System.out.println(b);
+    }
+}
