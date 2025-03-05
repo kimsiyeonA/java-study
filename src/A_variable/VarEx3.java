4640697dd65be714a293d9b37c3fddfc812993c4
kimsiyeonA <kakasska1212@gmail.com>
2025-03-05 15:10:14 +0900

    A_7Variable_ëª명규칙_문제풀이_정리완료

--- src/A_variable/VarEx3.java
+++ src/A_variable/VarEx3.java
@@ -6,6 +6,7 @@ public class VarEx3 {
         long l = 1000000000L;
         boolean b = true;
 
+        System.out.println("l = " + l); // soutv
         System.out.println(l);
         System.out.println(b);
     }
