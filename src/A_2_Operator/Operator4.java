f268c28ba75a0a5cc22ac0642beed37c1c8f1f95
kimsiyeonA <163291238+kimsiyeonA@users.noreply.github.com>
2025-03-07 23:59:52 +0900

    Update Operator4.java

--- src/A_2_Operator/Operator4.java
+++ src/A_2_Operator/Operator4.java
@@ -1,7 +1,7 @@
 package A_2_Operator;
 
 // 연산자 우선순위
-public class Operator4 {
+public class Operator4 { //
     public static void main(String[] args) {
         int sum3 = 2 * 2 + 3 * 3; // (2 * 2) + (3 * 3)
         int sum4 = (2 * 2) + (3 * 3); // sum3과 같다.
