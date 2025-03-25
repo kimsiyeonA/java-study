665705da9a255e2b0ec8cfdf671f47ff5b9e9439
kimsiyeonA <kakasska1212@gmail.com>
2025-03-25 15:16:17 +0900

    AWS indent

---
+++ src/awsJava/javaBasic/A003_indent.java
@@ -0,0 +1,13 @@
+package awsJava.javaBasic;
+
+public class A003_indent {// 들여쓰기
+
+    public static void main(String[] args) {
+        // file->settings->keymap-> eclipse ->format
+        // ctrl+shift+F
+
+        // 코드는 가독성이 좋아야한다. 그렇기에 들여쓰기를 사용하여 가독성을 높인다.
+        // 들여쓰기 하려면 tab 으로 맞출 수 있다.
+    }
+}
+
