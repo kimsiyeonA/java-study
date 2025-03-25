2f9b75d3fd8338ac1c971c44d76a710f1ee3b689
kimsiyeonA <kakasska1212@gmail.com>
2025-03-25 10:34:27 +0900

    A_2_ãOperator

---
+++ src/A_2_Operator/Operator5.java
@@ -0,0 +1,12 @@
+package A_2_Operator;
+
+// 증감연산자
+// ++ , --로 표현되며 변수의 값을 1만큼 증가시키거나 감소시킴
+// 값을 1씩 증가하거나 1씩 감소할 때가 아주 많기 때문에 편의 기능을 제공한다.
+public class Operator5 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        a = a + 1;
+    }
+}
