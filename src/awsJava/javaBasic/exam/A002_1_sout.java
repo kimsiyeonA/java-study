01075e280778765d72bfa09c2ce2047fc3ac18f9
kimsiyeonA <kakasska1212@gmail.com>
2025-03-25 14:55:38 +0900

    AWS sout exam

---
+++ src/awsJava/javaBasic/exam/A002_1_sout.java
@@ -0,0 +1,20 @@
+package awsJava.javaBasic.exam;
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+public class A002_1_sout {
+    public static void main(String[] args) {
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+        System.out.println("안녕하세요.");
+
+        //-------------------------
+        System.out.println("안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요.안녕하세요." +
+                "안녕하세요.안녕하세요.안녕하세요.안녕하세요.");
+    }
+}
