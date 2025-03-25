8be4d397672a6f542cada5a1dc522607180a0bff
kimsiyeonA <kakasska1212@gmail.com>
2025-03-25 13:16:28 +0900

    AWS sout

---
+++ src/awsJava/javaBasic/A002_sout.java
@@ -0,0 +1,29 @@
+package awsJava.javaBasic;
+
+public class A002_sout {// main 클래스
+    public static void main (String[] args){
+        // System.out 여기서 .은 of를 의미함.
+        // System.out.println(100); // 여기서 println(100); 와 같은 형태는 타 동사를 의미합니다.
+        // 사람.팔 > 사람의 팔 .-> of
+        // System.out.println(100); // 여기서 System.out 은 주어를 의미 합니다.
+        // System.out.println(100); // 여기서 100은 목적어 또는 보어 입니다.
+        // System.out.println(100); // 개발자가 "System.out" 라는 녀석에게 100 을 println 하라는 의미
+
+        // 프로그램의 시작점
+        System.out.println(1); // 실행순서 1 : 숫자는 " 로 감싸지 않아도 됩니다.
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("사");
+        // 프로그램의 끝점
+    }
+}
+
+/*
+ * System.out.println(Hello);
+ * 표준 출력(모니터) > 모니터에 보여주겠다.
+ * print + ln (line) > 한줄을 출력하겠다.
+ * 문장은  "" 으로 감싸져야 한다.
+ *
+ * 코드는 실행순서는 위 아래로, 왼쪽에서 오른쪽으로 실행된다.
+ * 한줄이 끝나지 않으면 다음줄이 실행할 수 없다.
+ */
