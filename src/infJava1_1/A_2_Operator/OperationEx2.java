151d6b8db70e6c64c674d6ca2f332f342d157ccf
kimsiyeonA <kakasska1212@gmail.com>
2025-05-28 00:20:23 +0900

    A_2_OperatorEx

---
+++ src/infJava1_1/A_2_Operator/OperationEx2.java
@@ -0,0 +1,16 @@
+package infJava1_1.A_2_Operator;
+
+public class OperationEx2 {
+    public static void main(String[] args) {
+        //다음 double 변수들을 선언하고 그 합과 평균을 구하는 출력 프로그램을 작성하세요.
+        double val1 = 1.5;
+        double val2 = 2.5;
+        double val3 = 3.5;
+
+        double sum = val1 + val2 + val3;
+        double average = sum / 3;
+
+        System.out.println("sum = " + sum);
+        System.out.println("average = " + average);
+    }
+}
