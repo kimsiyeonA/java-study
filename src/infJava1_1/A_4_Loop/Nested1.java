5565a6597bacdd565261abaa78c6173d7335c3ea
kimsiyeonA <kakasska1212@gmail.com>
2025-06-27 23:42:59 +0900

    A_4_Loop 중첩반복문

---
+++ src/infJava1_1/A_4_Loop/Nested1.java
@@ -0,0 +1,18 @@
+package infJava1_1.A_4_Loop;
+
+// 중첩 반복문
+// 반복문 내부에 또 반복문을 만들 수 있다
+// for, while 모두 가능하다.
+
+public class Nested1 {
+    public static void main(String[] args) {
+        for(int i = 0; i < 2; i++){
+            System.out.println("외부 for 시작 i:"+i);
+            for(int j = 0; j < 3; j++){
+                System.out.println("-> 내부 for "+i+"-"+j);
+            }
+            System.out.println("외부 for 종료 i:" + i);
+            System.out.println();
+        }
+    }
+}
