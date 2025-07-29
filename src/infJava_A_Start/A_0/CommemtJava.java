bbbdac43e4402f68e22522b44a0ee781389428a2
kimsiyeonA <kakasska1212@gmail.com>
2025-07-29 19:47:22 +0900

    A_0 주석

---
+++ src/infJava_A_Start/A_0/CommemtJava.java
@@ -0,0 +1,15 @@
+package infJava_A_Start.A_0;
+
+public class CommemtJava {
+    public static void main(String[] args) { //psvm+enter
+        System.out.println("hello java1"); 
+        // sout+enter // hello java1을 무시합니다 > 이줄은 무시됨 한줄 주석
+        // System.out.println("hello java2");
+
+        /* 여러줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        System.out.println("hello java5");
+         */
+    }
+}
