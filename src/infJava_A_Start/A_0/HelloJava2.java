9b267a9fd69600908e33b6613c9cb02ee3b71cf0
kimsiyeonA <kakasska1212@gmail.com>
2025-07-29 19:48:15 +0900

    A_0 Hello Java 2

---
+++ src/infJava_A_Start/A_0/HelloJava2.java
@@ -0,0 +1,9 @@
+package infJava_A_Start.A_0;
+
+public class HelloJava2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+        System.out.println("Hello World2");
+        System.out.println("Hello World3");
+    }
+}
