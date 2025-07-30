6b804a92d8d88f205100c4b80e6ef983e67d3df4
kimsiyeonA <kakasska1212@gmail.com>
2025-07-30 10:04:51 +0900

    A_1_Variable 변수 시작

---
+++ src/infJava_A_Start/A_1_Variable/Var1.java
@@ -0,0 +1,11 @@
+package infJava_A_Start.A_1_Variable;
+
+// 변수 시작
+
+public class Var1 {
+    public static void main(String[] args) {
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+        System.out.println(20); // 변경 10 -> 20
+    }
+}
