9c6f69b8d9729be21b79ec2f248f6e48592b463a
kimsiyeonA <kakasska1212@gmail.com>
2025-07-30 10:09:10 +0900

    A_1_Variable 변수의 선언

---
+++ src/infJava_A_Start/A_1_Variable/Var4.java
@@ -0,0 +1,14 @@
+package infJava_A_Start.A_1_Variable;
+
+// 변수의 선언
+// 데이터를 보관할 수 있는 컴퓨터의 메모리공간을 확보에서 그곳에 데이터를 저장할 수 있다.
+// 그리고 변수의 이름을 통해서 그 메모리 공간에 접근
+public class Var4 {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        int c, d; // 한번에 여러개를 선언할 때 ,를 사용하여 다양한 변수를 선언할 수 있다.
+    }
+}
