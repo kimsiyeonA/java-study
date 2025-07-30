14ecbdbfc76f3545459ae744f9a67f33910092a5
kimsiyeonA <kakasska1212@gmail.com>
2025-07-30 10:08:39 +0900

    A_1_Variable 변수 값 변경

---
+++ src/infJava_A_Start/A_1_Variable/Var3.java
@@ -0,0 +1,16 @@
+package infJava_A_Start.A_1_Variable;
+
+// 변수 값 변경
+
+public class Var3 {
+    public static void main(String[] args) {
+        int a;  // 변수 선언 (숫자 정수만 담을 수 있는그릇)
+        a = 10; // 변수 초기화 a(10)
+
+        System.out.println(a);
+
+        a = 50; // 변수의 값 변경 : a(10 -> 50) 10에 대한 내용은 제거가 됨
+
+        System.out.println(a);
+    }
+}
