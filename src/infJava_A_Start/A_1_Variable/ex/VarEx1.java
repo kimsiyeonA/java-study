981463cef0e57077c99145a2387fc1aa0ac934cc
kimsiyeonA <kakasska1212@gmail.com>
2025-07-31 21:14:02 +0900

    A_1_Variable 문제1

---
+++ src/infJava_A_Start/A_1_Variable/ex/VarEx1.java
@@ -0,0 +1,19 @@
+package infJava_A_Start.A_1_Variable.ex;
+
+// 문제1
+
+public class VarEx1 {
+    public static void main(String[] args) {
+
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+
+        int num1 = 4;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+    }
+}
