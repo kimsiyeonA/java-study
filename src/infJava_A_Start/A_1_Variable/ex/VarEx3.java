e4fd7e15546f989b746416b03dd9af12bcf09484
kimsiyeonA <kakasska1212@gmail.com>
2025-07-31 21:14:31 +0900

    A_1_Variable 문제3 - long, boolean 데이터 타입

---
+++ src/infJava_A_Start/A_1_Variable/ex/VarEx3.java
@@ -0,0 +1,14 @@
+package infJava_A_Start.A_1_Variable.ex;
+
+// 문제3 - long, boolean 데이터 타입
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long l = 1000000000L;
+        boolean b = true;
+
+        System.out.println("l = " + l); // soutv
+        System.out.println(l);
+        System.out.println(b);
+    }
+}
