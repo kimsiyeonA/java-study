06f58e37462ba20c35797d8bde9ab5c1b30945fc
kimsiyeonA <kakasska1212@gmail.com>
2025-07-31 21:14:12 +0900

    A_1_Variable 문제2

---
+++ src/infJava_A_Start/A_1_Variable/ex/VarEx2.java
@@ -0,0 +1,13 @@
+package infJava_A_Start.A_1_Variable.ex;
+
+// 문제2
+
+public class VarEx2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int sum = num1+num2;
+        System.out.println(sum);
+    }
+}
