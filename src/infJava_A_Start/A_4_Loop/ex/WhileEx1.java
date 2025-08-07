71114dd9bde5649220e16bd6adc532d346d7e912
kimsiyeonA <kakasska1212@gmail.com>
2025-08-07 12:47:52 +0900

    A_4_Loop WhileEx1 문제 - 자연수 출력 - while 문 버전

---
+++ src/infJava_A_Start/A_4_Loop/ex/WhileEx1.java
@@ -0,0 +1,22 @@
+package infJava_A_Start.A_4_Loop.ex;
+
+// 자연수 출력 - while 문 버전
+
+// 처음 10개의 자연수를 출력하는 프로그램을 작성해보세요.
+// 이때, count라는 변수를 사용해야 합니다.
+
+public class WhileEx1 {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count <= 10){
+            System.out.println(count);
+            count++;
+        }
+
+        // 강사 풀이
+        while(count <= 10){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
