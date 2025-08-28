35c1cc0b8c0b3b418f228b5e8b87510a7396148e
kimsiyeonA <kakasska1212@gmail.com>
2025-08-28 23:25:23 +0900

    A_3_Opp Account 문제 - 객체 지향 - 계좌 클래스- 내 풀이

---
+++ src/infJava_B_Basic/A_3_Opp/ex/Account.java
@@ -0,0 +1,19 @@
+package infJava_B_Basic.A_3_Opp.ex;
+
+// 객체 지향 - 계좌 클래스
+
+public class Account {
+    int balance;
+
+    void deposit(int amount){
+        balance += amount;
+    }
+
+    void withdraw(int amount){
+        if(balance < amount){
+            System.out.println("잔액 부족");
+        }else{
+            balance -= amount;
+        }
+    }
+}
