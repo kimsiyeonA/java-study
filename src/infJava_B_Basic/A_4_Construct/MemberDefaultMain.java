735656d9737582390bc5aae9b32204f4d12d18ed
kimsiyeonA <kakasska1212@gmail.com>
2025-09-02 16:21:31 +0900

    A_4_Construct MemberDefaultMain 기본 생성자 - 기본 생성자 참조 실행 클래스

---
+++ src/infJava_B_Basic/A_4_Construct/MemberDefaultMain.java
@@ -0,0 +1,9 @@
+package infJava_B_Basic.A_4_Construct;
+
+// 기본 생성자 - 기본 생성자 참조 실행 클래스
+
+public class MemberDefaultMain {
+    public static void main(String[] args) {
+        MemberDefault memberDefault = new MemberDefault();
+    }
+}
