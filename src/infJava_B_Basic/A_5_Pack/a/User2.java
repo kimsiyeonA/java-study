a3400e66b90780be3c956e471d0499c74fffcc2f
kimsiyeonA <kakasska1212@gmail.com>
2025-09-06 15:17:47 +0900

    A_5_Pack User2 패키지 사용 1 - import 사옹 - pack.a.User - 같은 패키지 다른 클래스

---
+++ src/infJava_B_Basic/A_5_Pack/a/User2.java
@@ -0,0 +1,9 @@
+package infJava_B_Basic.A_5_Pack.a;
+
+//  패키지 사용 1 - import - 같은 패키지 다른 클래스
+
+public class User2 {
+    public User2(){
+        System.out.println("패키지 infJava_B_Basic.A_5_Pack.a 회원2 생성");
+    }
+}
