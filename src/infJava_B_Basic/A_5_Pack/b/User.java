52388b92800a10b862936e21afb0aaec3aae968b
kimsiyeonA <kakasska1212@gmail.com>
2025-09-06 14:51:31 +0900

    A_5_Pack User 패키지 사용 2 - 클래스 이름 중복 - Pack.b.User.java

---
+++ src/infJava_B_Basic/A_5_Pack/b/User.java
@@ -0,0 +1,10 @@
+package infJava_B_Basic.A_5_Pack.b;
+
+// 패키지 사용2 - 클래스 이름 중복
+
+public class User {
+    public User(){
+        System.out.println("패키지 infJava_B_Basic.A_5_Pack.b 회원 생성");
+    }
+}
+
